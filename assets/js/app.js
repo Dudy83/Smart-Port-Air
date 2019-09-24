4bb961aaf78c2fa0d8ac0d8116fc87ae839f5dfc
Dudy83 <guillaume.zehren@live.fr>
2019-09-24 10:43:59 +0200

    add new images/css

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,6 +16,7 @@ $(document).ready(function () {
         $('.animated-icon2').toggleClass('open');
     });
     $('.third-button').on('click', function () {
+        
 
         $('.animated-icon3').toggleClass('open');
     });
@@ -29,11 +30,9 @@ window.addEventListener('scroll', () => scrollCardsAnimation());
 function scrollCardsAnimation() {
 
 let cardsContainer = document.querySelector('#cards-container');
-let elm = document.querySelector('#diams-port-container').offsetTop;
-
 
   if (window.scrollY >= 300)
-        cardsContainer.setAttribute('class', 'd-flex align-items-center justify-content-around loaded');
+        cardsContainer.setAttribute('class', 'd-flex align-items-stretch justify-content-around loaded');
   else return;
 
 }
@@ -46,7 +45,7 @@ let $dropdownContent = document.getElementById('dropdown-content');
 let $websiteContent = document.getElementById('main');
 let $websiteBrand = document.getElementById('website-brand');
 let $footer = document.getElementsByTagName('footer')[0];
-console.log($footer);
+
 
 $navLeaver.addEventListener('click', () => {
     $dropdown.setAttribute('class', 'dropdown-not-loaded');
@@ -69,9 +68,18 @@ $hamburger.addEventListener('click', () => {
 });
 
 
+
 //animation-logo removed
 let $animLogo = document.getElementById('animation-logo');
 
 window.addEventListener('load', setTimeout(() =>{
     $animLogo.setAttribute('class', 'not-loaded');
 }, 500));
+
+
+let iframe = document.getElementById("pollution-paca");
+
+let svg = iframe.contentWindow.document.getElementsByTagName("svg")[0];
+
+svg.style.setProperty('transform', 'translate3d(-656px, 255px, 0px)');
+
